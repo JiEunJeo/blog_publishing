c9acc9233c1b27c9d9328f33ba17732caa3f4b64
jieun <jieunjj01@gmail.com>
2024-01-28 16:09:52 +0900

    로딩 느린 거 해결하고자 수정

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
-document.addEventListener("scroll", function () {
-  var gotopElement = document.querySelector(".gotop");
+document.addEventListener("DOMContentLoaded", function () {
+  // 이벤트 리스너 및 함수 호출
+  document.addEventListener("scroll", function () {
+    var gotopElement = document.querySelector(".gotop");
 
-  if (window.scrollY > 80) {
-    gotopElement.classList.add("active");
-  } else {
-    gotopElement.classList.remove("active");
-  }
+    if (window.scrollY > 80) {
+      gotopElement.classList.add("active");
+    } else {
+      gotopElement.classList.remove("active");
+    }
+  });
 });
 
 function scrollToTop() {
